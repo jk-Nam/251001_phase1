46663e6af546a13135a7c7338ac7f851df850736
jk-Nam <skawnsrn94@gmail.com>
2025-10-01 14:45:09 +0900

    step07

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express"); // express 안에 있는 이미 구현되어
 const cors = require("cors"); // 설치한 의존성 패키지 cors를 불러오기
 const dotenv = require("dotenv"); // 설치한 의존성 패키지 dotenv를 불러오기
 const { createClient } = require("@supabase/supabase-js"); // 구조분해 할당
+const { GoogleGenAI } = require("@google/genai");
 
 dotenv.config(); // .env -> KEY => SUPABASE_KEY
 // NODE -> pocess.env (환경변수) // cf. env file
@@ -42,8 +43,56 @@ app.get("/plans", async (req, res) => {
   res.json(data);
 });
 
+// 1. SDK
+// 2. API Key <- .env
+// 3. 여러 단계를 프롬프팅 -> (값) => (프롬프트) => (결과값)
 app.post("/plans", async (req, res) => {
   const plan = req.body;
+  // ai를 통해
+  // npm install @google/genai
+  const ai = new GoogleGenAI({}); // GEMINI_API_KEY 알아서 인식해줌
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: `
+    [장소] ${plan.destination}
+    [목적] ${plan.purpose}
+    [인원수] ${plan.people_count}
+    [시작일] ${plan.start_date}
+    [종료일] ${plan.end_date}`,
+    config: {
+      responseMimeType: "application/json",
+      responseSchema: {
+        type: "object",
+        properties: {
+          prompt: {
+            type: "string",
+          },
+        },
+        required: ["prompt"],
+      },
+      systemInstruction: [
+        // { text: "제공받은 정보를 바탕으로 여행 계획을 짜되, 300자 이내로." },
+        {
+          text: `제공받은 정보를 바탕으로 최적의 여행 계획을 세우기 위한 프롬프트를 작성해줘. 응답은 JSON 형식으로 {"prompt": "프롬프트 내용" 형식으로 작성해줘.}`,
+        },
+      ],
+      // structured output
+    },
+  });
+  const { prompt } = JSON.parse(response.text);
+  console.log("prompt", prompt);
+  const response2 = await ai.models.generateContent({
+    model: "gemini-2.5-flash-lite", // 모델을 상대적으로 약한 모델로...
+    contents: prompt,
+    config: {
+      systemInstruction: [
+        {
+          text: "프롬프트에 따라 작성하되, 300자 이내의 마크다운이 아닌 평문으로.",
+        },
+      ],
+    },
+  });
+  plan.ai_suggestion = response2.text;
   const { error } = await supabase.from("tour_plan").insert(plan);
   if (error) {
     return res.status(400).json({ error: error.message });
